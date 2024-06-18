028c87379f097c592d2b5dd16cbab7b055b46908
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2024-06-18 11:49:34 +0530

    Time: 4 ms (33.21%), Space: 9.4 MB (49.41%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,32 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int end, vector<int>& arr) {
+    int arr[101]; 
+
+    int solve(vector<int>& nums, int i, int end) {
         if (i > end)
             return 0;
         if (arr[i] != -1)
             return arr[i];
-        
-        int steal = nums[i] + solve(nums, i + 2, end, arr);
-        int skip = solve(nums, i + 1, end, arr);
-        
+
+        int steal = nums[i] + solve(nums, i + 2, end);
+        int skip = solve(nums, i + 1, end);
+
         return arr[i] = max(steal, skip);
     }
-    
+
     int rob(vector<int>& nums) {
         int n = nums.size();
         
+        if (n == 0)
+            return 0;
         if (n == 1)
             return nums[0];
-        if (n == 2)
-            return max(nums[0], nums[1]);
         
-        vector<int> arr1(n, -1); 
-        vector<int> arr2(n, -1); 
+        memset(arr, -1, sizeof(arr));
+        int max1 = solve(nums, 0, n - 2);
+        memset(arr, -1, sizeof(arr));
+        int max2 = solve(nums, 1, n - 1);
 
-        
-        int max1 = solve(nums, 0, n - 2, arr1);
-        int max2 = solve(nums, 1, n - 1, arr2);
-
-        
         return max(max1, max2);
     }
 };
