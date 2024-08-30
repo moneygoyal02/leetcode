00e140aa2163855231fd9a856456e03c397f5071
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2024-08-30 17:22:53 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (10.93%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,12 +1,34 @@
 class Solution {
 public:
+    
+    int solve(int n , vector<int>&dp){
+        
+         if(n<=1)
+             return n;
+        
+        if(dp[n]!=-1)
+            return dp[n];
+        
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        
+    };
     int fib(int n) {
         if(n<=1)
             return n;
         
-        return fib(n-1) + fib(n-2);
+        vector <int> dp(n+1,-1);
+        
+        return solve(n,dp);
     }
 };
 
 
 // Amazon ✯   Microsoft ✯   Adobe ✯   Apple ✯   Facebook ✯   Oracle   Goldman Sachs   Zoom   Google  
+
+
+// int fib(int n) {
+//         if(n<=1)
+//             return n;
+        
+//         return fib(n-1) + fib(n-2);
+//     }
