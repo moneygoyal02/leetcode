3e4e626c65dca6dd0b190d34b0ae7dac9c583c1b
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2024-08-30 17:32:28 +0530

    Time: 3 ms (66.34%), Space: 7.3 MB (64.91%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -32,3 +32,50 @@ public:
         
 //         return fib(n-1) + fib(n-2);
 //     }
+
+
+// USING BOTTOM UP DP 
+
+//     int fib(int n) {
+        
+//         if(n <= 1)
+//             return n;
+        
+//         int t[n+1];
+//         //State definition
+//         //t[i] = ith Fibonacci Number
+        
+//         t[0] = 0;
+//         t[1] = 1;
+        
+//         for(int i = 2; i<=n; i++) {
+//             t[i] = t[i-1] + t[i-2];
+//         }
+        
+//         return t[n];
+        
+//     }
+
+
+// (Constant Space Complexity)
+
+//   int fib(int n) {
+//         if( n<=1)
+//             return n;
+        
+        
+//         int c;
+//         int a = 0, b = 1;
+        
+//         for(int i = 1; i<n; i++) {
+            
+//             c = a+b;
+            
+//             a = b;
+//             b = c;
+             
+//         }
+        
+        
+//         return c;
+//     }
