966918d4c9176e9a22ae8fd8711f71cf3d532ad2
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2024-08-31 12:20:55 +0530

    Time: 4 ms (33.96%), Space: 12.3 MB (5.79%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -1,28 +1,45 @@
 class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
-        unordered_map<int, int> count;
+        vector <int> n(2001,0);
         
-      
-        for (auto &it : arr) {
-            count[it]++;
+        for(auto &it: arr){
+            n[it+1000]++;
         }
         
-        unordered_set<int> uni;
-   
-        for (auto &it : count) {
-            int occur = it.second;
-            
-            if (uni.find(occur) != uni.end()) {
-                return false; 
-            }
-            
-            uni.insert(occur); 
+        sort(begin(n),end(n));
+        
+        for(int i=1; i<2001;i++){
+            if( n[i]!=0 && n[i] == n[i-1]) 
+                return false;
         }
         
-        return true; 
+        return true;
     }
 };
 
+//  Amazon ✯   Adobe ✯   Facebook ✯   Apple ✯   Bloomberg ✯   Google  
 
-//  Amazon ✯   Adobe ✯   Facebook ✯   Apple ✯   Bloomberg ✯   Google 
+
+// bool uniqueOccurrences(vector<int>& arr) {
+//         unordered_map<int, int> count;
+        
+      
+//         for (auto &it : arr) {
+//             count[it]++;
+//         }
+        
+//         unordered_set<int> uni;
+   
+//         for (auto &it : count) {
+//             int occur = it.second;
+            
+//             if (uni.find(occur) != uni.end()) {
+//                 return false; 
+//             }
+            
+//             uni.insert(occur); 
+//         }
+        
+//         return true; 
+//     }
