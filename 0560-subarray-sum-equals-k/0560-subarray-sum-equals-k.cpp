f4fa7633a2f76e65c56772ecb3d2ebff6236238f
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-02-22 00:38:35 +0530

    Update 0560-subarray-sum-equals-k.cpp

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
          int sum = 0, count = 0;
-        unordered_map<int,int> prevSum;
+        unordered_map<int,int> prevSum;  // (prefix sum) -> (frequency)
         prevSum[0]=1;
         
         for(int i = 0; i<nums.size();i++){
             sum +=nums[i];
+             // If sum - k exists in map, add its frequency to count
             if(prevSum.find(sum-k) != prevSum.end()){
                 count += prevSum[sum-k];
             }
+             // Increment frequency of current sum
             prevSum[sum]++;
            
             
