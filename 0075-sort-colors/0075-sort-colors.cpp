a65310b66f3c6f68cbdce8f1ab22191661cd7ff4
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-02-22 18:12:36 +0530

    Time: 0 ms (100%), Space: 11.8 MB (31.72%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-
-        int low = 0 ;
-        int high = arr.size()-1;
-        int mid = 0;
+    void sortColors(vector<int>& nums) {
+        int low=0, high=nums.size()-1, mid=0;
 
         while(mid<=high){
-            if(arr[mid] == 0){
-                swap(arr[mid],arr[low]);
+            if(nums[mid] == 0){
+                swap(nums[mid],nums[low]);
                 low++;
                 mid++;
             }
-
-            else if (arr[mid] == 2){
-                swap(arr[mid],arr[high]);
+            else if(nums[mid] == 2){
+                swap(nums[mid],nums[high]);
                 high--;
             }
             else{
-                swap(arr[mid],arr[low]);
+
                 mid++;
             }
-        }
-        
+        }        
     }
 };
-
-//The time complexity of this solution is O(n) where n is the number of elements in the input array. This is because we are iterating through the array only once.
