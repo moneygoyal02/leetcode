16a1405cf916f616a8e6f96b5cd9c367c0bf1ba0
Money Goyal <138269416+moneygoyal02@users.noreply.github.com>
2025-02-24 22:22:11 +0530

    Time: 0 ms (100%), Space: 59.2 MB (91.26%) - LeetHub

---
+++ 1046-max-consecutive-ones-iii/1046-max-consecutive-ones-iii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestOnes(vector<int>& nums, int k) {
+       int l = 0;
+       int r = 0;
+       int maxLen = 0;       
+       int zeroes = 0;
+       while(r < nums.size()){
+        if(nums[r]== 0) zeroes++;
+        while(zeroes>k){
+            if(nums[l] == 0) zeroes--;
+            l++;
+        }
+        maxLen = max(maxLen,r-l+1);
+        r++;
+       }
+       return maxLen;
+    }
+};
